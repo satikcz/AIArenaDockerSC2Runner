03bbddc891241d68dde749e2953a4553c7be6b27
satik <satan.lord@seznam.cz>
2023-08-04 01:28:29 +0200

    Clonner to use older version

--- DockerSC2Runner/BootstrapGitClonner.cs
+++ DockerSC2Runner/BootstrapGitClonner.cs
@@ -9,29 +9,45 @@
         {
             try
             {
-                //Process.Start("git", $"clone {repoPath}");
-                using Process p = new Process();
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.FileName = "git";
-                p.StartInfo.WorkingDirectory = workingDir;
-                p.StartInfo.CreateNoWindow = true;
-
-                if (!Directory.Exists("local-play-bootstrap"))
                 {
-                    Console.WriteLine($"Clonning bootstrap from {repoPath}, please wait...");
-                    p.StartInfo.Arguments = $"clone {repoPath} --recurse-submodules";
-                }
-                else
-                {
-                    Console.WriteLine($"Pulling latest bootstrap version...");
-                    p.StartInfo.Arguments = "pull --recurse-submodules";
+                    using Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.FileName = "git";
+                    p.StartInfo.WorkingDirectory = workingDir;
+                    p.StartInfo.CreateNoWindow = true;
+
+                    if (!Directory.Exists("local-play-bootstrap"))
+                    {
+                        Console.WriteLine($"Clonning bootstrap from {repoPath}, please wait...");
+                        p.StartInfo.Arguments = $"clone {repoPath} --recurse-submodules";
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Pulling latest bootstrap version...");
+                        p.StartInfo.Arguments = $"pull --recurse-submodules";
+                    }
+
+                    p.Start();
+                    p.WaitForExit();
+                    var output = p.StandardOutput.ReadToEnd();
+                    Console.WriteLine($"Bootstrap result: {output}");
                 }
 
-                p.Start();
-                p.WaitForExit();
-                var output = p.StandardOutput.ReadToEnd();
-                Console.WriteLine($"Bootstrap result: {output}");
+                {
+                    using Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.FileName = "git";
+                    p.StartInfo.WorkingDirectory = workingDir + "/local-play-bootstrap";
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.Arguments = $"reset --hard 0693cf9098a0ea35546f999677a85ec874bb6173";
+
+                    p.Start();
+                    p.WaitForExit();
+                    var output = p.StandardOutput.ReadToEnd();
+                    Console.WriteLine($"Set to 0693cf9098a0ea35546f999677a85ec874bb6173");
+                }
             }
             catch (Exception ex) 
             {
